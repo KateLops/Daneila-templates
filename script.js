270ab96a651fe75f9fe9bb75fc7c86039a5b6a5f
Kate Lops <112887410+KateLops@users.noreply.github.com>
2022-11-09 11:36:20 +0700

    1.2

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 //Инициализируем Swiper
+
 new Swiper('.image-slider', {
 
     navigation:{
+
+        //Отображение стрелочек вперёд назад
+
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
     },
+
+    //Изменение кол-ва отображаемых слайдов
+    
     slidesPerView: 5,
 });
 
